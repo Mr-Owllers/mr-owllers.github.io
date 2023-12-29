a5f730f1b96d2ca216c1104ad5f11f47b04504ec
Mr-Owllers <deeponbasu@gmail.com>
2023-12-29 15:00:06 +0530

    fixed mobile ui

--- ts/script.ts
+++ ts/script.ts
@@ -12,27 +12,6 @@ window.onload = function() {
     });
 }
 
-// const wrapper = document.getElementById("bubble-wrapper");
-
-// const animateBubble = (x: any) => {
-//     const bubble = document.createElement("div");
-
-//     bubble.className = "bubble";
-//     bubble.style.left = `${x}px`;
-
-//     wrapper.appendChild(bubble);
-//     if (window.innerWidth < 659) {
-//         bubble.style.opacity = "0";
-//     }
-//     else {
-//         bubble.style.opacity = "1";
-//     }
-
-//     setTimeout(() => wrapper.removeChild(bubble), 2000);
-// }
-
-// window.onmousemove = (e: { clientX: any; }) => animateBubble(e.clientX);
-
 if(localStorage.getItem("cookie") == 'true') {
     document.getElementById("cookie-banner").style.display = 'none';
     // make buttons clickable
